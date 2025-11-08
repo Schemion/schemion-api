ea53f648c9fa705e49494a148b5f04963f76e3d3
Владислав <103439608+lilvladick@users.noreply.github.com>
2025-11-08 11:42:33 +0300

    Update rabbitmq_publisher.py

--- app/core/messaging/rabbitmq_publisher.py
+++ app/core/messaging/rabbitmq_publisher.py
@@ -5,10 +5,10 @@ from app.config import settings
 
 class RabbitMQPublisher:
     def __init__(self):
-        self.connection = pika.BlockingConnection(
+        self._connection = pika.BlockingConnection(
             pika.URLParameters(settings.RABBITMQ_URL)
         )
-        self.channel = self.connection.channel()
+        self.channel = self._connection.channel()
 
     def publish(self, queue_name: str, message: dict):
         self.channel.queue_declare(queue=queue_name, durable=True)
@@ -20,5 +20,13 @@ class RabbitMQPublisher:
             properties=pika.BasicProperties(delivery_mode=2),
         )
 
+    def close(self):
+        try:
+            if self._connection and self._connection.is_open:
+                self._connection.close()
+        except Exception as e:
+            #TODO: Заменить на лог вместо принта
+            print(f"Error closing RabbitMQ connection: {e}")
+
     def __del__(self):
-        self.connection.close()
+        self.close()
